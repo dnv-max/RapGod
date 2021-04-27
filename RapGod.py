b7046a3b285e1ecb28d530362c43e8ae01f045d8
dnv-max <81434516+dnv-max@users.noreply.github.com>
2021-04-27 03:38:30 -0400

    Update RapGod.py

--- RapGod.py
+++ RapGod.py
@@ -1,15 +1,5 @@
 import pyautogui
 import time
-print(" _  ___           _                 _       _     ")
-print("| |/ (_)_ __   __| | __ _  ___ __ _| |_ ___| |__  ")
-print("| ' /| | '_ \ / _` |/ _` |/ __/ _` | __/ __| '_ \ ")
-print("| . \| | | | | (_| | (_| | (_| (_| | || (__| | | |")
-print("|_|\_\_|_| |_|\__,_|\__,_|\___\__,_|\__\___|_| |_|")
-print("  ")
-print("  ")
-print("  ")
-print("  ")
-print("  ")
 print("Please Choose what you want to spam (you will have ten seconds to go to the destination of spam")
 print("A] full song                                 B] fastest verse ")
 a = str(input("Enter the digit :  ") 
